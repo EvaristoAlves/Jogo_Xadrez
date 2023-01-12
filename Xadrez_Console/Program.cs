e9b3463e558757ea6f052f457a633cf0fe1b0880
Evaristo Alves <evaristocarvalho34@gmail.com>
2023-01-12 14:53:54 -0300

    criada a classe posição

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Tabuleiro;
+
+Posicao P = new Posicao(3, 4);
+
+Console.WriteLine("A posição :" + P);
+
+Console.ReadLine();
